8e3b97b21be939e8ac974f1a58a9963a63ed1724
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2017-11-16 23:41:53 +1100

    Added racing car

---
+++ Assets/Scripts/Car/CarPaint.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarPaint : MonoBehaviour
+{
+
+    public MeshRenderer[] m_carPaintMeshRenderes;
+    public string[] m_excludeMatNames;
+
+    public void ColorCar(Color color)
+    {
+        foreach (MeshRenderer mr in m_carPaintMeshRenderes)
+        {
+            foreach (Material mat in mr.materials)
+            {
+                if (!m_excludeMatNames.Contains(mat.name))
+                {
+                    mat.color = color;
+                }
+            }
+        }
+    }
+}
