1d7c8e7f2458e15fbf0ba22f6bcdc6dea299c2c2
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2017-11-21 08:21:35 +1100

    Custom editor to swap materials

---
+++ Assets/Editor/SSUtils/SceneMaterialSwapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SceneMaterialSwapper : EditorWindow {
+
+	[MenuItem("Tools/SceneMaterialSwapper")]
+	public static void Window()
+    {
+        EditorWindow.GetWindow(typeof(SceneMaterialSwapper));
+
+        Debug.Log("It should have appeared!");
+    }
+	
+	// Update is called once per frame
+	string test = "";
+    public void OnGUI()
+    {
+        test = EditorGUILayout.TextField ("Text Field", test );
+    }
+}
