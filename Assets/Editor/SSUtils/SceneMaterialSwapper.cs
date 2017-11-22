d075db6c6ac13544fbecfa12b968f00c86373650
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2017-11-23 06:37:48 +1100

    WIP material swapper

--- Assets/Editor/SSUtils/SceneMaterialSwapper.cs
+++ Assets/Editor/SSUtils/SceneMaterialSwapper.cs
@@ -14,9 +14,47 @@ public class SceneMaterialSwapper : EditorWindow {
     }
 	
 	// Update is called once per frame
-	string test = "";
-    public void OnGUI()
+	static string tagStr = "";
+	public Material matFind = null;
+	public Material matReplace = null;
+    void OnGUI()
     {
-        test = EditorGUILayout.TextField ("Text Field", test );
+		EditorGUILayout.BeginVertical();
+        if (GUILayout.Button("Swap materials for selection"))
+        {
+            SwapMaterials();
+        }
+		EditorGUILayout.EndVertical();
+
+        Rect lastRect = GUILayoutUtility.GetLastRect();
+
+        matFind = (Material)EditorGUI.ObjectField(new Rect(3, lastRect.y + lastRect.height + 3, position.width - 6, 20), "Find material", matFind, typeof(Material), true);
+        matReplace = (Material)EditorGUI.ObjectField(new Rect(3, lastRect.y + lastRect.height + 23, position.width - 6, 20), "Replace material", matReplace, typeof(Material), true);
+    }
+
+	void OnInspectorUpdate()	
+    {
+        Repaint();
+    }
+
+    void SwapMaterials()
+    {
+        if (matFind == null || matReplace == null) {
+            Debug.LogError("Either 'Find Material' or 'Replace Material' not assigned.");
+            return;
+        }
+
+        foreach (GameObject go in Selection.gameObjects)
+        {
+            MeshRenderer[] mrs = go.GetComponentsInChildren<MeshRenderer>() as MeshRenderer[];
+            foreach (MeshRenderer mr in mrs) {
+                foreach(Material mat in mr.sharedMaterials) {
+                    // Start here
+                    if (mat.name == "YellowLight") {
+                        Debug.Log("material name : " + mat.name);
+                    }
+                }
+            }
+        }
     }
 }
