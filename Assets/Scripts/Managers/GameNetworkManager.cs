995214f4688d575ffe77cf122c08a93469243197
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2018-03-03 09:26:14 +1100

    Custom game network manager

---
+++ Assets/Scripts/Managers/GameNetworkManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+public class GameNetworkManager : NetworkManager
+{
+    [Header("Scene camera properties")]
+    [SerializeField]
+    Transform sceneCamera;
+    [SerializeField]
+    float cameraRotationRadius = 24f;
+    [SerializeField]
+    float cameraRoataitonSpeed = 3f;
+    [SerializeField]
+    bool canRotate = true;
+
+    float m_ratation;
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        canRotate = false;
+    }
+
+    public override void OnStartHost()
+    {
+        canRotate = false;
+    }
+
+    public override void OnStopClient()
+    {
+        canRotate = true;
+    }
+
+    public override void OnStopHost()
+    {
+        canRotate = true;
+    }
+
+    void Update()
+    {
+        if (!canRotate)
+            return;
+
+        m_ratation += cameraRoataitonSpeed * Time.deltaTime;
+        if (m_ratation >= 360f)
+            m_ratation -= 360f;
+
+        sceneCamera.position = Vector3.zero;
+        sceneCamera.rotation = Quaternion.Euler(0f, m_ratation, 0f);
+        sceneCamera.Translate(0f, cameraRotationRadius, -cameraRotationRadius);
+        sceneCamera.LookAt(Vector3.zero);
+    }
+}
