a6d539454127d41c417b914376c7c690309d5e38
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2018-03-07 23:09:02 +1100

    Work in progress

--- Assets/Scripts/Managers/AR/ARManager.cs
+++ Assets/Scripts/Managers/AR/ARManager.cs
@@ -6,19 +6,22 @@ public class ARManager : MonoBehaviour
 {
     public Transform arRoot;
     public Transform levelArt;
+    public ArUiManager arUiManager;
+
+    private GameObject m_debugPlaneGoRef;
+
     public void FitToSurface()
     {
         GameObject[] debugPlanes = GameObject.FindGameObjectsWithTag("DebugPlane") as GameObject[];
 
         if (debugPlanes.Length > 0)
         {
-            GameObject go = debugPlanes[Random.Range(0, debugPlanes.Length - 1)];
-            Debug.Log(go.transform.position);
-            arRoot.position = go.transform.position;
-            // levelArRoot.rotation = go.transform.rotation;
-            // levelArRoot.localScale = go.transform.localScale;
-            float size = Mathf.Max(go.transform.localScale.x, go.transform.localScale.z);
-            size *= 2;
+            m_debugPlaneGoRef = debugPlanes[Random.Range(0, debugPlanes.Length - 1)];
+            Debug.Log(m_debugPlaneGoRef.transform.position);
+            arRoot.position = m_debugPlaneGoRef.transform.position;
+            arRoot.rotation = m_debugPlaneGoRef.transform.rotation;
+            float size = Mathf.Max(m_debugPlaneGoRef.transform.localScale.x, m_debugPlaneGoRef.transform.localScale.z);
+            size *= arUiManager.gameboardScaleSetting;
             arRoot.localScale = new Vector3(size, size, size);
 
             foreach (GameObject debugGo in debugPlanes)
@@ -28,8 +31,22 @@ public class ARManager : MonoBehaviour
         }
     }
 
+    public void AdjustGameboard(float scaleMultiplier, float rotation)
+    {
+        if (!m_debugPlaneGoRef)
+            return;
+
+        float size = Mathf.Max(m_debugPlaneGoRef.transform.localScale.x, m_debugPlaneGoRef.transform.localScale.z);
+        size *= scaleMultiplier;
+        arRoot.localScale = new Vector3(size, size, size);
+        arRoot.Rotate(Vector3.up, rotation);
+    }
+
     public void DisplayDebugPlanes()
     {
+        if (!m_debugPlaneGoRef)
+            return;
+
         GameObject[] debugPlanes = GameObject.FindGameObjectsWithTag("DebugPlane") as GameObject[];
         foreach (GameObject go in debugPlanes)
         {
--- Assets/Scripts/UI/AR/ArUiManager.cs
+++ Assets/Scripts/UI/AR/ArUiManager.cs
@@ -7,6 +7,9 @@ public class ArUiManager : MonoBehaviour
     public GameObject MenuBtn;
     public GameObject OptionsPanel;
     public GameObject TouchControls;
+    public float gameboardScaleSetting;
+    public float gameboardRotateSetting;
+    public ARManager aRManager;
 
     public void MenuBtnPressed()
     {
@@ -19,6 +22,20 @@ public class ArUiManager : MonoBehaviour
     {
         OptionsPanel.SetActive(false);
         MenuBtn.SetActive(true);
-        TouchControls.SetActive(true);
+        // TouchControls.SetActive(true);
+    }
+
+    public void ScaleGameBoard(float sliderVlaue)
+    {
+        Debug.Log("ScaleGameBoard :" + sliderVlaue);
+        gameboardScaleSetting = sliderVlaue;
+        aRManager.AdjustGameboard(gameboardScaleSetting, gameboardRotateSetting);
+    }
+
+    public void RotateGameBoard(float sliderVlaue)
+    {
+        Debug.Log("RotateGameBoard :" + sliderVlaue);
+        gameboardRotateSetting = sliderVlaue;
+        aRManager.AdjustGameboard(gameboardScaleSetting, gameboardRotateSetting);
     }
 }
